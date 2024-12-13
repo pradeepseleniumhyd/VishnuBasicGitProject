aeb6cb9cf31df3c843564b68a6c9e51e980204c9
unknown <pradeepseleniumhyd@gmail.com>
2024-12-13 08:05:12 +0530

    Runner file is added

---
+++ src/test/java/runner/loginRunner2 - Copy.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features = "C:\\Users\\User\\Documents\\Vishnu Class\\VishnuProject\\src\\test\\java\\features",
+		glue= {"stepdef"},
+		dryRun = true,
+		plugin= {"pretty","html:Vishnu.html","json:vishnu.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		publish = true,
+		monochrome = true
+		)
+public class loginRunner2 extends AbstractTestNGCucumberTests {
+
+}
